bcd7fd27387d156f88a99d8274b039e5885473b2
Duke-Saurabh <ds8123495@example.com>
2024-10-11 02:27:26 +0530

    abc

--- urban-residential-properties/src/components/Login.jsx
+++ urban-residential-properties/src/components/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
     e.preventDefault();
     console.log('logging clicked')
     try {
-      const response = await axios.post('http://localhost:3000/api/auth/login', { email, password });
+      const response = await axios.post('/api/auth/login', { email, password });
       
       console.log('Login successful', response.data);
       // Show a welcome message
