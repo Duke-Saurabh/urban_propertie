78ac864baf4d038527971bd398d41be8ad811e93
Duke-Saurabh <ds8123495@example.com>
2024-10-11 11:09:26 +0530

    abc

--- urban-residential-properties/src/components/Header.jsx
+++ urban-residential-properties/src/components/Header.jsx
@@ -1,10 +1,11 @@
 // src/components/Header.jsx
 import React, { useState } from 'react';
 import './Header.css';
+import { useAuth } from '../context/authContext';
 
 const Header = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
-
+  const {isAuthenticated}=useAuth
   const toggleNav = () => {
     setIsNavOpen((prev) => !prev);
   };
@@ -20,11 +21,12 @@ const Header = () => {
         
         <nav className={`navigation ${isNavOpen ? 'open' : 'close'}`}>
           <ul className="navigation-list">
-            <li className="navigation-item"><a href="/home">Home</a></li>
-            <li className="navigation-item"><a href="/AddProperty">Sell Properties</a></li>
-            <li className="navigation-item"><a href="/listings">Listings</a></li>
-            <li className="navigation-item"><a href="/profile">Profile</a></li>
-            <li className="navigation-item"><a href="/login">Login</a></li>
+           {isAuthenticated && ( <li className="navigation-item"><a href="/home">Home</a></li>)}
+           {isAuthenticated && (<li className="navigation-item"><a href="/AddProperty">Sell Properties</a></li>)}
+           {isAuthenticated && ( <li className="navigation-item"><a href="/listings">Listings</a></li>)}
+           {isAuthenticated && ( <li className="navigation-item"><a href="/profile">Profile</a></li>)}
+           {isAuthenticated || (<li className="navigation-item"><a href="/login">Login</a></li>)}
+           {isAuthenticated && (<li className="navigation-item"><a href="/logut">Logout</a></li>)}
             <li className="navigation-item"><a href="/signup">Signup</a></li>
           </ul>
         </nav>
